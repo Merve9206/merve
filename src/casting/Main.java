e2e991ce05e5d768e2c47dd3ba2262b13f85d9d2
Merve <altunolukmerve@gmail.com>
2023-12-08 19:42:39 -0500

    added new commit

---
+++ src/casting/Main.java
@@ -0,0 +1,14 @@
+package casting;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double result = a / b;
+        float normalResult = (float) result;
+        System.out.println(normalResult);
+    }
+}
